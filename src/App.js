3e4182c4d6e7712cab545e486180ad0cba02c637
unknown <lp411301@gmail.com>
2023-04-01 15:26:15 -0300

    no build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import React from "react";
 import './App.css';
+import emojipedia from "./emojipedia";
+import Emojis from "./components/Emojis";
+
+// 1 create entry component
+// 2 create props to replace hardcode Data
+// 3 import the emojipedia
+// 4 map through emojipedia array and render entry component 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        <span>Emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+       {emojipedia.map(item => <Emojis key={item.id} emoji={item.emoji} name={item.name} description={item.meaning}/>)}
+      </dl>
     </div>
   );
 }
+
+// truncate string
+console.log(emojipedia.map(item => item.meaning.substring(0,20)));
 
 export default App;
---
+++ src/emojipedia.js
@@ -0,0 +1,68 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "💪",
+      name: "Tense Biceps",
+      meaning:
+        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+    },
+    {
+      id: 2,
+      emoji: "🙏",
+      name: "Person With Folded Hands",
+      meaning:
+        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+    },
+    {
+      id: 3,
+      emoji: "🤣",
+      name: "Rolling On The Floor, Laughing",
+      meaning:
+        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+    },
+    {
+      id: 4,
+      emoji: "🙈",
+      name: "See-No-Evil Monkey",
+      meaning:
+        "Monkey with two hands in front of its eyes. Closes its eyes to the world so as not to see anything bad or cruel, or is embarrassed. Mizaru is one of the three wise monkeys. In a sense, ignoring bad things." 
+    },
+    {
+      id: 5,
+      emoji: "🤪",
+      name: "Crazy Face",
+      meaning:
+        "Something is insanely funny. The mood is exuberant - you are totally silly and crazy. Totally freaking out with enthusiasm or joy. Maybe someone has even told an indecent joke." 
+    },
+    {
+      id: 6,
+      emoji: "🥴",
+      name: "Face with unequal eyes and wavy mouth",
+      meaning:
+        "Eyes half open, half closed, slanted mouth, flushed cheeks. The emoji has a confused facial expression: due to tiredness or the currently confused emotional world. Also known as drunken face." 
+    },
+    {
+      id: 7,
+      emoji: "🔥",
+      name: "Fire",
+      meaning:
+        "You're as hot as a volcano! The fire emoji is often used as a compliment and also in connection with “to be enlightened”, or for spicy food." 
+    },
+    {
+      id: 8,
+      emoji: "🍆",
+      name: "Eggplant",
+      meaning:
+        "The eggplant is associated with the male sex. It is one of the sex symbols. Is hated and loved on Instagram #freetheeggplant." 
+    },
+    {
+      id: 9,
+      emoji: "😎",
+      name: "Face With Sunglasses",
+      meaning:
+        "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach." 
+    }
+  ];
+  
+  export default emojipedia;
+  
